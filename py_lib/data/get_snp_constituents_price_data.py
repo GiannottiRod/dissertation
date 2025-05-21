135f9f23915b568f88c3d95eda04e2543a4f48a1
RodrigoGiannotti <rodrigo.giannotti@atmospherecapital.com.br>
2025-05-21 19:03:43 -0300

    Historical data gatherv2

--- py_lib/data/get_snp_constituents_price_data.py
+++ py_lib/data/get_snp_constituents_price_data.py
@@ -4,10 +4,10 @@ import QuantLib as ql
 import requests
 from datetime import datetime as dt
 from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 from tqdm import tqdm
 import time
 import json
-import pickle
 
 from py_lib.data.snp_constituents import get_snp500_constituents
 from py_lib.data import snp_constituents
@@ -18,14 +18,12 @@ NYSE_holidays = [dt(x.year(), x.month(), x.dayOfMonth()) for x
 
 NYSE_cal = pd.offsets.CustomBusinessDay(holidays=NYSE_holidays, n=1, weekmask='Mon Tue Wed Thu Fri')
 
-TIINGO_API_KEY_1 = '5345ade70a633f4e7f9fae15b85df135729c8540'
-TIINGO_API_KEY_2 = 'be73adc34512bad596ed2b4126ac600eb2fd688c'
-TIINGO_API_KEY_3 = '00ece7cc32b7ec36f4196d24e88745f8a03578e5'
+TIINGO_API_KEY_1 = '00ece7cc32b7ec36f4196d24e88745f8a03578e5'
 
 # json_file_path = 'py_lib/data/DailyPricesRaw.json'
 json_file_path = '.\\DailyPricesRaw.json'
 
-MAX_HOURLY_REQUESTS = 45
+MAX_HOURLY_REQUESTS = 10000
 
 DATA_START = '2003-12-31 00:00:00'
 DATA_END = '2024-12-31 00:00:00'
@@ -47,21 +45,25 @@ ticker_date_range = {
     for t in daily_constituents.columns
 }
 
-with open(json_file_path, 'r') as f_handle:
-    available_data = json.load(f_handle)
+try:
+    with open(json_file_path, 'r') as f_handle:
+        available_data = json.load(f_handle)
+except FileNotFoundError:
+    available_data = []
 
 already_got_tickers = [x['ticker'] for x in available_data]
 
-api_keys = [TIINGO_API_KEY_1, TIINGO_API_KEY_2, TIINGO_API_KEY_3]
+api_keys = [TIINGO_API_KEY_1]
 available_keys = len(api_keys)
 key_to_use = 0
-requests_made = 0
+requests_made = 1
 progress = 0
 
 data_compendium = available_data
-tickers_failed = []
+tickers_failed = ['CBS', 'JCP', 'JP', 'TUP', 'WFT', 'DF', 'ESV', 'FB', 'MNK', 'ENDP', 'HFC', 'FRC']
+already_got_tickers += tickers_failed
 
-posssible_repplaces = {
+possible_replaces = {
 
 }
 
@@ -74,7 +76,7 @@ for ticker, (start_date, end_date) in ticker_date_range.items():
         url = f"https://api.tiingo.com/tiingo/daily/{ticker}/prices"
         headers = {'Content-Type': 'application/json'}
         params = {
-            "startDate": start_date,
+            "startDate": pd.to_datetime(DATA_START) - relativedelta(years=2),
             "endDate": end_date,
             "token": api_key
         }
@@ -104,7 +106,7 @@ for ticker, (start_date, end_date) in ticker_date_range.items():
         now = dt.now()
         seconds_elapsed = (now - start_time).seconds
         completion_pct = progress / len(ticker_date_range)
-        expected_end = now + timedelta(seconds=(seconds_elapsed / completion_pct))
+        expected_end = now + timedelta(seconds=(seconds_elapsed / completion_pct) - seconds_elapsed)
         print(f'{completion_pct * 100:.3} % Done')
         print(f'{seconds_elapsed} Seconds Elapsed')
         print(f'Expected End: {expected_end.strftime('%m-%d %H:%M')}')
@@ -113,3 +115,6 @@ for ticker, (start_date, end_date) in ticker_date_range.items():
 with open(json_file_path, 'w') as f_handle:
     json.dump(data_compendium, f_handle, default=str)
 
+print(f"Failed tickers: {tickers_failed}")
+
+# last run time elapsed, paid plan, no rate limit: aprox.
